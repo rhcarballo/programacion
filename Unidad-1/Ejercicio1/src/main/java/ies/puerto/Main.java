ffc14ba4fd9a76847704e80de1bfd9113398fbbe
rhcarballo <robertohernandezcarballo@gmail.com>
2023-09-25 14:59:08 +0100

    Se incluyen los cambios del ejercicio 1

---
+++ Unidad-1/Ejercicio1/src/main/java/ies/puerto/Main.java
@@ -0,0 +1,47 @@
+package ies.puerto;
+
+import java.util.Scanner;
+
+/**
+ * Clase que realiza el ejercicio numero 1 de las tareas
+ */
+public class Main {
+    public static void main(String[] args){
+
+        int valorA;
+        int valorB;
+
+        Scanner lectura = new Scanner (System.in);
+
+
+        //Bloque 2: verifica si los valores son iguales
+
+        do{
+            //Bloque 1: Pide por pantalla valor y asigna valor
+
+            System.out.println("Ingrese el valor de la variable A: ");
+            valorA = lectura.nextInt();
+            System.out.println("valor de la variable A:"+valorA);
+
+            System.out.println("Ingrese el valor de la variable B: ");
+            valorB = lectura.nextInt();
+            System.out.println("valor de la variable B:"+valorB);
+
+        } while (valorA == valorB);
+
+        //Bloque 3: verifica si A > B
+
+        if(valorA == valorB) {
+            System.out.printf("Los valores introducidos son iguales");
+        } else {
+            if (valorA > valorB){
+                System.out.printf("El valor de A es mayor");
+            } else {
+                System.out.printf("El valor de B es mayor");
+            }
+        }
+
+        //Bloque 4: Muestra por pantalla el valor mayor
+
+    }
+}
