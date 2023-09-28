44883c28feccc4b349ef41aa4feab40300cf2c77
rhcarballo <robertohernandezcarballo@gmail.com>
2023-09-28 12:13:05 +0100

    añadimos programas 5, 6 y 7 sin hacer

--- Unidad-1/tareas/tareaX/tarea2/README.md
+++ Unidad-1/tareas/tareaX/tarea2/README.md
@@ -52,4 +52,8 @@ Inicio
 
 
 
+# Ejercicio 3
+
+Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos entre el 1 y el 10, es decir, 1 + 2 + 3 + …. + 10. Utilia un buble __for__ y un bucle __while__.
+
 </div>
--- Unidad-1/tareas/tareaX/tarea2/src/Ejercicio7.java
+++ Unidad-1/tareas/tareaX/tarea2/src/Ejercicio7.java
@@ -6,9 +6,6 @@ public class Ejercicio1 {
 
     /**
      * Descripcion del algoritmo
-     * Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos entre el 1 y el 10, 
-     * es decir, 1 + 2 + 3 + …. + 10. 
-     * Utilia un buble __for__ y un bucle __while__.
      * @param args
      */
     public static void main(String[] args) {
