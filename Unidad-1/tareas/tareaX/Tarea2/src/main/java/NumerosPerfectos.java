e176db352bc2d2f039b2c5fa04328fadf859730f
rhcarballo <robertohernandezcarballo@gmail.com>
2023-10-06 18:44:33 +0100

    Empezamos tarea 3

--- Unidad-1/tareas/tareaX/Tarea2/src/main/java/NumerosPerfectos.java
+++ Unidad-1/tareas/tareaX/Tarea2/src/main/java/NumerosPerfectos.java
@@ -2,14 +2,23 @@ public class NumerosPerfectos {
 
     /**
      * Descripcion del algoritmo
-     * Escribe un programa en Java que imprima el patrón siguiente:
-     * 1
-     * 2 3
-     * 4 5 6
-     * 7 8 9 10
+     * programa que encuentre e imprima todos los números perfectos en un rango dado (por ejemplo, entre 1 y 1000).
      */
     public static void main(String[] args) {
 
+        System.out.println("Números perfectos entre 1 y 1000");
+        int sumadedivisores = 0;
 
+        for(int n = 1; n <= 1000; n++) {
+            for(int divisor = 1; divisor <= n/2; divisor++) {
+                if(n % divisor == 0) {
+                    sumadedivisores += divisor;
+                }
+            }
+            if(sumadedivisores == n) {
+                System.out.println(n);
+            }
+            sumadedivisores = 0;
+        }
     }
 }
--- Unidad-1/tareas/tareaX/Tarea3/src/main/java/Ejercicio1.java
+++ Unidad-1/tareas/tareaX/Tarea3/src/main/java/Ejercicio1.java
@@ -10,6 +10,9 @@ public class Ejercicio1 {
      */
     public static void main(String[] args) {
 
+        for (int n = 1; n <= 10, n++){
+            System.out.println(n)
+        }
 
     }
 }
